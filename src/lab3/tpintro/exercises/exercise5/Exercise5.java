686205784738a064a6fbfa1ca214c1bbae3c4e3c
axelfantini <axel.fantini@outlook.com>
2021-03-20 01:27:20 -0300

    Exercise 5 & Exercise 6

--- src/lab3/tpintro/exercises/exercise5/Exercise5.java
+++ src/lab3/tpintro/exercises/exercise5/Exercise5.java
@@ -1,7 +1,14 @@
 package lab3.tpintro.exercises.exercise5;
-
 public class Exercise5 {
     public void StartExercise() {
-
+        int B = 5;
+        System.out.println("B = " + B);
+        String response = IsEven(B) ? "even" : "odd";
+        System.out.println(B + " is an " + response + " number");
     }
+
+    private boolean IsEven(int number) {
+        return number % 2 == 0;
+    }
+
 }
--- src/lab3/tpintro/exercises/exercise6/Exercise6.java
+++ src/lab3/tpintro/exercises/exercise6/Exercise6.java
@@ -2,6 +2,14 @@ package lab3.tpintro.exercises.exercise6;
 
 public class Exercise6 {
     public void StartExercise() {
+        int B = 4;
+        System.out.println("B = " + B);
+        String response = B == 0 ? "neutral" : (IsPositive(B) ? "positive" : "negative");
+        System.out.println(B + " is a " + response + " number");
+    }
 
+    private boolean IsPositive(int number)
+    {
+        return number > 0;
     }
 }
